7b242229db7f736af01593f4ab741cfcb7cfa135
linfeng <1029776125@qq.com>
2023-06-11 10:13:41 +0800

    定义InstantiationStrategy

---
+++ src/main/java/com/tw/minispring/beans/factory/support/InstantiationStrategy.java
@@ -0,0 +1,13 @@
+package com.tw.minispring.beans.factory.support;
+
+import com.tw.minispring.BeansException;
+import com.tw.minispring.beans.factory.config.BeanDefinition;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/11 10:12 上午
+ */
+public interface InstantiationStrategy {
+
+    Object instantiate(BeanDefinition beanDefinition) throws BeansException;
+}
