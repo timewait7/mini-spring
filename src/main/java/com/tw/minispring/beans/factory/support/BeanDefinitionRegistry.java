4733b26662778e5b68e6603922e8988ec491b22e
timewait7 <1029776125@qq.com>
2023-06-11 09:20:23 +0800

    定义DefaultListableBeanFactory

--- src/main/java/com/tw/minispring/beans/factory/support/BeanDefinitionRegistry.java
+++ src/main/java/com/tw/minispring/beans/factory/support/BeanDefinitionRegistry.java
@@ -4,5 +4,5 @@ import com.tw.minispring.beans.factory.config.BeanDefinition;
 
 public interface BeanDefinitionRegistry {
 
-    void register(String beanName, BeanDefinition beanDefinition);
+    void registerBeanDefinition(String beanName, BeanDefinition beanDefinition);
 }
