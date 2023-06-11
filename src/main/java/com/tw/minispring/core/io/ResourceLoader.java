50dcf3107fc558446d33ae2264a01a737ece663d
linfeng <1029776125@qq.com>
2023-06-11 22:29:52 +0800

    定义ResourceLoader

---
+++ src/main/java/com/tw/minispring/core/io/ResourceLoader.java
@@ -0,0 +1,10 @@
+package com.tw.minispring.core.io;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/11 10:29 下午
+ */
+public interface ResourceLoader {
+
+    Resource getResource(String location);
+}
