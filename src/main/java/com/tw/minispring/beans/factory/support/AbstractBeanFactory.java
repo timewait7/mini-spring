3557a43faa2a5c71330536bef568167fcba1bea0
timewait7 <1029776125@qq.com>
2023-06-11 09:10:29 +0800

    定义AbstractBeanFactory

--- src/main/java/com/tw/minispring/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/com/tw/minispring/beans/factory/support/AbstractBeanFactory.java
@@ -1,4 +1,23 @@
 package com.tw.minispring.beans.factory.support;
 
-public class AbstractBeanFactory {
+import com.tw.minispring.BeansException;
+import com.tw.minispring.beans.factory.BeanFactory;
+import com.tw.minispring.beans.factory.config.BeanDefinition;
+
+public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+
+    @Override
+    public Object getBean(String beanName) throws BeansException {
+        Object bean = getSingleton(beanName);
+        if (bean != null) {
+            return bean;
+        }
+
+        BeanDefinition beanDefinition = getBeanDefinition(beanName);
+        return createBean(beanName, beanDefinition);
+    }
+
+    protected abstract Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException;
+
+    protected abstract BeanDefinition getBeanDefinition(String beanName) throws BeansException;
 }
