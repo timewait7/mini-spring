db68d153f574b51495303f117eab977c1d02ce3e
linfeng <1029776125@qq.com>
2023-06-11 11:17:55 +0800

    为doCreateBean方法添加填充属性逻辑

--- src/main/java/com/tw/minispring/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/tw/minispring/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,6 +1,8 @@
 package com.tw.minispring.beans.factory.support;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.tw.minispring.BeansException;
+import com.tw.minispring.PropertyValue;
 import com.tw.minispring.beans.factory.config.BeanDefinition;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
@@ -16,6 +18,8 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         Object bean;
         try {
             bean = createBeanInstance(beanDefinition);
+            // 为bean填充属性
+            applyPropertyValues(beanName, bean, beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
@@ -26,6 +30,19 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 
     protected Object createBeanInstance(BeanDefinition beanDefinition) {
         return getInstantiationStrategy().instantiate(beanDefinition);
+    }
+
+    protected void applyPropertyValues(String beanName, Object bean, BeanDefinition beanDefinition) {
+        try {
+            for (PropertyValue propertyValue : beanDefinition.getPropertyValues().getPropertyValues()) {
+                String name = propertyValue.getName();
+                Object value = propertyValue.getValue();
+                // 通过反射设置属性
+                BeanUtil.setFieldValue(bean, name, value);
+            }
+        } catch (Exception e) {
+            throw new BeansException("Error setting property values for bean: " + beanName, e);
+        }
     }
 
     private InstantiationStrategy getInstantiationStrategy() {
