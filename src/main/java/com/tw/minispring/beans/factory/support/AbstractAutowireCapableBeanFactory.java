2884d7eb6e39cd26ce4bd1c327cdf14b2f36925d
linfeng <1029776125@qq.com>
2023-06-11 10:33:15 +0800

    更新AbstractAutowireCapableBeanFactory

--- src/main/java/com/tw/minispring/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/tw/minispring/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -5,21 +5,34 @@ import com.tw.minispring.beans.factory.config.BeanDefinition;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
+    private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
+
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
         return doCreateBean(beanName, beanDefinition);
     }
 
     protected Object doCreateBean(String beanName, BeanDefinition beanDefinition) {
-        Class beanClass = beanDefinition.getBeanClass();
         Object bean;
         try {
-            bean = beanClass.newInstance();
+            bean = createBeanInstance(beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
 
         addSingleton(beanName, bean);
         return bean;
+    }
+
+    protected Object createBeanInstance(BeanDefinition beanDefinition) {
+        return getInstantiationStrategy().instantiate(beanDefinition);
+    }
+
+    private InstantiationStrategy getInstantiationStrategy() {
+        return instantiationStrategy;
+    }
+
+    private void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+        this.instantiationStrategy = instantiationStrategy;
     }
 }
