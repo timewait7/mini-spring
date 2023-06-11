a5e20167d018b808b954c55c324bc66fca4af0ac
linfeng <1029776125@qq.com>
2023-06-11 10:17:20 +0800

    定义SimpleInstantiationStrategy

---
+++ src/main/java/com/tw/minispring/beans/factory/support/SimpleInstantiationStrategy.java
@@ -0,0 +1,32 @@
+package com.tw.minispring.beans.factory.support;
+
+import com.tw.minispring.BeansException;
+import com.tw.minispring.beans.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/11 10:14 上午
+ */
+public class SimpleInstantiationStrategy implements InstantiationStrategy {
+
+
+    /**
+     * 简单的bean实例化策略，根据bean的无参构造函数实例化对象
+     *
+     * @param beanDefinition
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition) throws BeansException {
+        Class beanClass = beanDefinition.getBeanClass();
+        try {
+            Constructor constructor = beanClass.getDeclaredConstructor();
+            return constructor.newInstance();
+        } catch (Exception e) {
+            throw new BeansException("Failed to instantiate [" + beanClass.getName() + "]", e);
+        }
+    }
+}
