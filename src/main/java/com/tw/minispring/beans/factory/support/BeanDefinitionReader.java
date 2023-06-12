8557e3653d641a97f4fa3f30ecc83058742dc29d
linfeng <1029776125@qq.com>
2023-06-12 08:41:33 +0800

    定义BeanDefinitionReader

---
+++ src/main/java/com/tw/minispring/beans/factory/support/BeanDefinitionReader.java
@@ -0,0 +1,22 @@
+package com.tw.minispring.beans.factory.support;
+
+import com.tw.minispring.beans.BeansException;
+import com.tw.minispring.core.io.Resource;
+import com.tw.minispring.core.io.ResourceLoader;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/12 8:38 上午
+ */
+public interface BeanDefinitionReader {
+
+    BeanDefinitionRegistry getRegistry();
+
+    ResourceLoader getResourceLoader();
+
+    void loadBeanDefinitions(Resource resource) throws BeansException;
+
+    void loadBeanDefinitions(String location) throws BeansException;
+
+    void loadBeanDefinitions(String[] locations) throws BeansException;
+}
