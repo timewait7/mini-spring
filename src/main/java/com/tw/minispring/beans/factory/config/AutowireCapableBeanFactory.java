3e6173ca1fd5b4b08ad6465424c11faae9a19988
linfeng <1029776125@qq.com>
2023-06-12 08:35:35 +0800

    定义AutowireCapableBeanFactory

---
+++ src/main/java/com/tw/minispring/beans/factory/config/AutowireCapableBeanFactory.java
@@ -0,0 +1,10 @@
+package com.tw.minispring.beans.factory.config;
+
+import com.tw.minispring.beans.factory.BeanFactory;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/12 8:35 上午
+ */
+public interface AutowireCapableBeanFactory extends BeanFactory {
+}
