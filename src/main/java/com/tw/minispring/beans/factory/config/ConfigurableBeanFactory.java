596cac5d83e5aba840a242448d8be9e3e26b27e3
linfeng <1029776125@qq.com>
2023-06-12 08:34:49 +0800

    定义ConfigurableBeanFactory

---
+++ src/main/java/com/tw/minispring/beans/factory/config/ConfigurableBeanFactory.java
@@ -0,0 +1,11 @@
+package com.tw.minispring.beans.factory.config;
+
+import com.tw.minispring.beans.factory.HierarchicalBeanFactory;
+import com.tw.minispring.beans.factory.support.BeanDefinitionRegistry;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/12 8:33 上午
+ */
+public interface ConfigurableBeanFactory extends HierarchicalBeanFactory, BeanDefinitionRegistry {
+}
