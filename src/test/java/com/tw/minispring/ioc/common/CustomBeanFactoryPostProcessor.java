d1e1b8fb6035fbadffedf821e7788b6529cf324f
linfeng <1029776125@qq.com>
2023-06-17 13:54:24 +0800

    CustomBeanFactoryPostProcessor

---
+++ src/test/java/com/tw/minispring/ioc/common/CustomBeanFactoryPostProcessor.java
@@ -0,0 +1,22 @@
+package com.tw.minispring.ioc.common;
+
+import com.tw.minispring.beans.BeansException;
+import com.tw.minispring.beans.PropertyValue;
+import com.tw.minispring.beans.PropertyValues;
+import com.tw.minispring.beans.factory.ConfigurableListableBeanFactory;
+import com.tw.minispring.beans.factory.config.BeanDefinition;
+import com.tw.minispring.beans.factory.config.BeanFactoryPostProcessor;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/17 1:21 下午
+ */
+public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessorBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition beanDefinition = beanFactory.getBeanDefinition("person");
+        PropertyValues propertyValues = beanDefinition.getPropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("name", "ivy"));
+    }
+}
