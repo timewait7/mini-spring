b14058509ce8e7769b5a8e92ac5e1dc4e67ceaad
linfeng <1029776125@qq.com>
2023-06-18 21:56:49 +0800

    AdvisedSupport

---
+++ src/main/java/com/tw/minispring/aop/AdvisedSupport.java
@@ -0,0 +1,40 @@
+package com.tw.minispring.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/18 9:54 下午
+ */
+public class AdvisedSupport {
+
+    private TargetSource targetSource;
+
+    private MethodInterceptor methodInterceptor;
+
+    private MethodMatcher methodMatcher;
+
+    public TargetSource getTargetSource() {
+        return targetSource;
+    }
+
+    public void setTargetSource(TargetSource targetSource) {
+        this.targetSource = targetSource;
+    }
+
+    public MethodInterceptor getMethodInterceptor() {
+        return methodInterceptor;
+    }
+
+    public void setMethodInterceptor(MethodInterceptor methodInterceptor) {
+        this.methodInterceptor = methodInterceptor;
+    }
+
+    public MethodMatcher getMethodMatcher() {
+        return methodMatcher;
+    }
+
+    public void setMethodMatcher(MethodMatcher methodMatcher) {
+        this.methodMatcher = methodMatcher;
+    }
+}
