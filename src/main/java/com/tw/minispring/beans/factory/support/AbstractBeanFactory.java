20ff08f82af85f772866aabdf6885ad9a9f6c50b
linfeng <1029776125@qq.com>
2023-06-18 11:17:27 +0800

    更新AbstractBeanFactory#getBean，通过FactoryBean#getObject创建bean

--- src/main/java/com/tw/minispring/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/com/tw/minispring/beans/factory/support/AbstractBeanFactory.java
@@ -4,23 +4,50 @@ import com.tw.minispring.beans.BeansException;
 import com.tw.minispring.beans.factory.config.BeanDefinition;
 import com.tw.minispring.beans.factory.config.BeanPostProcessor;
 import com.tw.minispring.beans.factory.config.ConfigurableBeanFactory;
+import com.tw.minispring.beans.factory.xml.FactoryBean;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory {
 
     private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<>();
 
+    private final Map<String, Object> factoryBeanObjectCache = new HashMap<>();
+
     @Override
     public Object getBean(String beanName) throws BeansException {
-        Object bean = getSingleton(beanName);
-        if (bean != null) {
-            return bean;
+        Object sharedInstance = getSingleton(beanName);
+        if (sharedInstance != null) {
+            return getObjectForBeanInstance(sharedInstance, beanName);
         }
 
         BeanDefinition beanDefinition = getBeanDefinition(beanName);
-        return createBean(beanName, beanDefinition);
+        Object bean = createBean(beanName, beanDefinition);
+        return getObjectForBeanInstance(bean, beanName);
+    }
+
+    protected Object getObjectForBeanInstance(Object beanInstance, String beanName) {
+        Object object = beanInstance;
+        if (beanInstance instanceof FactoryBean) {
+            FactoryBean factoryBean = (FactoryBean) beanInstance;
+            try {
+                if (factoryBean.isSingleton()) {
+                    object = factoryBeanObjectCache.get(beanName);
+                    if (object == null) {
+                        object = factoryBean.getObject();
+                        factoryBeanObjectCache.put(beanName, object);
+                    }
+                } else {
+                    object = factoryBean.getObject();
+                }
+            } catch (Exception e) {
+                throw new BeansException("FactoryBean threw exception on object[" + beanName + "] creation", e);
+            }
+        }
+        return object;
     }
 
     @Override
