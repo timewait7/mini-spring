70b2103c1cb9e6191a7c0c379c4b8cec9279ceca
linfeng <1029776125@qq.com>
2023-06-18 10:31:32 +0800

    更新AbstractAutowireCapableBeanFactory

--- src/main/java/com/tw/minispring/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/tw/minispring/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -36,13 +36,17 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         // 注册有销毁方法的bean
         registerDisposableBeanIfNecessary(beanName, bean, beanDefinition);
 
-        addSingleton(beanName, bean);
+        if (beanDefinition.isSingleton()) {
+            addSingleton(beanName, bean);
+        }
         return bean;
     }
 
     protected void registerDisposableBeanIfNecessary(String beanName, Object bean, BeanDefinition beanDefinition) {
-        if (bean instanceof DisposableBean && StrUtil.isNotEmpty(beanDefinition.getDestroyMethodName())) {
-            registerDisposableBean(beanName, new DisposableBeanAdapter(bean, beanName, beanDefinition));
+        if (beanDefinition.isSingleton()) {
+            if (bean instanceof DisposableBean && StrUtil.isNotEmpty(beanDefinition.getDestroyMethodName())) {
+                registerDisposableBean(beanName, new DisposableBeanAdapter(bean, beanName, beanDefinition));
+            }
         }
     }
 
