d68b7a877841ca0f0b21371c475076dbf4f16bf7
linfeng <1029776125@qq.com>
2023-06-18 13:27:00 +0800

    定义ApplicationContextEvent

---
+++ src/main/java/com/tw/minispring/context/ApplicationContextEvent.java
@@ -0,0 +1,21 @@
+package com.tw.minispring.context;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/18 1:25 下午
+ */
+public class ApplicationContextEvent extends ApplicationEvent {
+    /**
+     * Constructs a prototypical Event.
+     *
+     * @param source The object on which the Event initially occurred.
+     * @throws IllegalArgumentException if source is null.
+     */
+    public ApplicationContextEvent(Object source) {
+        super(source);
+    }
+
+    public final ApplicationContext getApplicationContext() {
+        return (ApplicationContext) getSource();
+    }
+}
