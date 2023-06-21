59789d993922a80d09f892973b5dd951078210bd
linfeng <1029776125@qq.com>
2023-06-21 14:36:50 +0800

    JdkDynamicAopProxy、ReflectiveMethodInvocation

---
+++ src/main/java/com/tw/minispring/aop/framework/JdkDynamicAopProxy.java
@@ -0,0 +1,35 @@
+package com.tw.minispring.aop.framework;
+
+import com.tw.minispring.aop.AdvisedSupport;
+import org.aopalliance.intercept.MethodInterceptor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/18 9:58 下午
+ */
+public class JdkDynamicAopProxy implements AopProxy, InvocationHandler {
+
+    private final AdvisedSupport advised;
+
+    public JdkDynamicAopProxy(AdvisedSupport advised) {
+        this.advised = advised;
+    }
+
+    @Override
+    public Object getProxy() {
+        return Proxy.newProxyInstance(getClass().getClassLoader(), advised.getTargetSource().getTargetClass(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (advised.getMethodMatcher().matches(method, advised.getTargetSource().getTargetClass().getClass())) {
+            MethodInterceptor methodInterceptor = advised.getMethodInterceptor();
+            return methodInterceptor.invoke(new ReflectiveMethodInvocation(advised.getTargetSource().getTarget(), method, args));
+        }
+        return method.invoke(advised.getTargetSource().getTarget(), args);
+    }
+}
