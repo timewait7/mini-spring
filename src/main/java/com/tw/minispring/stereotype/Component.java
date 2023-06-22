a46d1ac53a2576e63851116573952ec5a2486221
linfeng <1029776125@qq.com>
2023-06-22 17:26:58 +0800

    Component注解

---
+++ src/main/java/com/tw/minispring/stereotype/Component.java
@@ -0,0 +1,13 @@
+package com.tw.minispring.stereotype;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author: linfeng
+ * @Date: 2023/6/22 5:25 下午
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Component {
+}
